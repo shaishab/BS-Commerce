15db62ba63c6aa12d0b1612963c4ba2e092d2c07
Shaishab <shaishab.cse@gmail.com>
2016-04-28 17:10:30 +0600

    Added services for admin panel
    Added all services for admin panel from previous project.

--- public/modules/shopAdmin/services/shop.admins.authentication.service.js
+++ public/modules/shopAdmin/services/shop.admins.authentication.service.js
@@ -1,14 +1,19 @@
 'use strict';
 
-// Authentication service for admin variables
-angular.module('shopAdmin').factory('AdminAuthentication', [
+//Global service for global variables
+angular.module('shopAdmin').factory('Global', [
+
     function() {
         var _this = this;
-
         _this._data = {
-            user: window.user
+            user: window.user,
+            authenticated: false,
+            isAdmin: false
         };
-
+        if (window.user && window.user.roles) {
+            _this._data.authenticated = window.user.roles.length;
+            _this._data.isAdmin = window.user.roles.indexOf('admin') !== -1;
+        }
         return _this._data;
     }
 ]);
