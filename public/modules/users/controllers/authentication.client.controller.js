2363eb057c48dc7f0f0c543f0eabcfbcd9880911
Shaishab <shaishab.cse@gmail.com>
2016-05-02 13:04:49 +0600

    Reload home page after signin and use Global service in admin dashboard and clint controller for user authentication

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
+angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication','$window',
+	function($scope, $http, $location, Authentication, $window) {
 		$scope.authentication = Authentication;
 
 		// If user is signed in then redirect back home
@@ -26,6 +26,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 
 				// And redirect to the index page
 				$location.path('/');
+				$window.location.reload();
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
