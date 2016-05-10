dd6fd8b87942852a3f6642c17a08e085fe99ed5c
Shaishab <shaishab.cse@gmail.com>
2016-05-10 19:13:20 +0600

    set lodash as global for lightweight theme

--- public/themes/lightweight/lightweight.js
+++ public/themes/lightweight/lightweight.js
@@ -3,8 +3,8 @@
 // Use applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('lightweight', ['ngAnimate', 'ui.bootstrap']);
 
-//angular.module('lightweight')
-//    .config(['$viewPathProvider', function($viewPathProvider) {
-//        $viewPathProvider.override('modules/core/views/hoem.client.view.html', 'themes/lightweight/views/index.html');
-//
-//    }]);
+angular.module('lightweight')
+    .constant('_', window._)
+    .run(function ($rootScope) {
+        $rootScope._ = window._;
+    });
