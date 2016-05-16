abb88076bdd0da781a16ecbd9b80fa408f35a7a7
Shaishab <shaishab.cse@gmail.com>
2016-05-16 17:00:57 +0600

    Add add to cart from featured list page

--- public/themes/lightweight/directives/product/product.featured.item.directive.js
+++ public/themes/lightweight/directives/product/product.featured.item.directive.js
@@ -1,27 +1,14 @@
 'use strict';
 
-angular.module('lightweight').directive('productFeaturedItem', ['$timeout', 'Global', 'ProductService',
-    function($timeout, Global, ProductService) {
+angular.module('lightweight').directive('productFeaturedItem', ['$rootScope','$timeout', 'Global', 'ProductService','UserService','CartService',
+    function($rootScope, $timeout, Global, ProductService, UserService, CartService) {
         return{
             restrict: 'AE',
             replace: true,
             templateUrl: 'themes/lightweight/views/product/product-featured-item.html',
             link: function(scope, element, attrs){
 
-                //var updateCartStatus = function(){
-                //    cartService.getCart()
-                //        .$promise
-                //        .then(function(cart){
-                //            _.forEach(scope.products, function(product){
-                //                var indexInCart = _.findIndex(cart.items, function(item){
-                //                    return item.product._id === product._id;
-                //                });
-                //
-                //                product.addedToCart = indexInCart >= 0;
-                //            });
-                //        });
-                //
-                //};
+                scope.isBusy = false;
 
                 scope.getFeaturedProducts = function() {
                     var query = {
@@ -32,30 +19,34 @@ angular.module('lightweight').directive('productFeaturedItem', ['$timeout', 'Glo
                         .$promise
                         .then(function(promise) {
                             scope.products = promise.products;
-                            //console.log(scope.products[0].photos[0]);
-                            //updateCartStatus();
                         });
                 };
                 scope.getFeaturedProducts();
 
-                //scope.toggleCartStatus = function(product, event){
-                //    if(!Global.authenticated) {
-                //        product.notAuthenticate = true;
-                //        $timeout(function() {
-                //            product.notAuthenticate = false;
-                //        },1000);
-                //        return;
-                //    }
-                //    event.preventDefault();
-                //    product.addedToCart = !(product.addedToCart);
-                //
-                //    if(product.addedToCart) {
-                //        cartService.addToCart(product, 1);
-                //    }else{
-                //        cartService.removeFromCart(product);
-                //    }
-                //
-                //};
+                scope.addToCart = function(product, event){
+
+                    var item = {product:product._id, quantity: 1};
+
+                    if(!Global.authenticated) {
+                        scope.isBusy = true;
+
+                        UserService.createGuestUser().$promise.then(function(data) {
+                            CartService.addToCart({item: item}).$promise.then(function(cartResponse) {
+                                scope.isBusy = false;
+                                $rootScope.$emit('cart:updated');
+                            });
+                        });
+                    } else {
+                        scope.isBusy = true;
+                        CartService.addToCart({item: item}).$promise.then(function(data) {
+                            scope.isBusy = false;
+                            $rootScope.$emit('cart:updated');
+                        });
+                    }
+                    event.preventDefault();
+
+                };
+
             }
         };
     }
