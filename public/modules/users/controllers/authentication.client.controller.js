9bc4a6d51cd22f07fb95471e2e114ad506bd1b14
Shaishab <shaishab.cse@gmail.com>
2016-05-17 15:34:41 +0600

    Add global service for user module and update authentication controller to check guest or registered user.

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication','$window',
+angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Global','$window',
 	function($scope, $http, $location, Authentication, $window) {
 		$scope.authentication = Authentication;
 
 		// If user is signed in then redirect back home
-		if ($scope.authentication.user) $location.path('/');
+		if ($scope.authentication.user && $scope.authentication.isRegistered) $location.path('/');
 
 		$scope.signup = function() {
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
