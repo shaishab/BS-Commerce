b44a79031b73000228b6c1d848b77386a8d6d015
Shaishab <shaishab.cse@gmail.com>
2016-05-18 21:42:12 +0600

    Add some routes for client side

--- public/themes/lightweight/routes/lightweight.routes.js
+++ public/themes/lightweight/routes/lightweight.routes.js
@@ -27,6 +27,30 @@ angular.module('lightweight').config(['$stateProvider', '$urlRouterProvider',
                 parent:'master',
                 controller: 'ProductDetailsController',
                 templateUrl: 'themes/lightweight/views/product/product-details.html'
+            })
+            .state('Cart', {
+                url: '/cart',
+                controller: 'CartController',
+                templateUrl: 'themes/lightweight/views/cart/cart.html'
+            })
+            .state('Checkout', {
+                url: '/checkout',
+                controller: 'CheckoutController',
+                templateUrl: 'themes/lightweight/views/cart/checkout.html'
+            })
+            .state('CheckoutSuccess', {
+                url: '/checkout/success/:orderId',
+                controller: 'CheckoutSuccessController',
+                templateUrl: 'themes/lightweight/views/cart/checkout-success.html'
+            })
+            .state('emptyCart', {
+                url: '/cart/empty',
+                templateUrl: 'themes/lightweight/views/cart/empty-cart.html'
+            })
+            .state('orderDetails/:orderId', {
+                url: '/order/:orderId',
+                controller: 'OrderDetailsController',
+                templateUrl: 'themes/lightweight/views/order/order-details.html'
             });
     }
 ]);
