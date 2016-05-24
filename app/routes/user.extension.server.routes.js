7f8cd755200ae07fced3cefb8026b52ff430c19e
Shaishab <shaishab.cse@gmail.com>
2016-05-24 19:45:42 +0600

    Update user routes for server side and some unused function removed

--- app/routes/user.extension.server.routes.js
+++ app/routes/user.extension.server.routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var extensionUserController = require('../controllers/user.extension.server.controller');
+var extensionUserController = require('../controllers/user.extension.server.controller'),
+    exUserPasswordController = require('../controllers/userExtension/password.server.controller'),
+    exUserAuthenticationController = require('../controllers/userExtension/authentication.server.controller'),
+    exUserProfileController = require('../controllers/userExtension/profile.server.controller');
 
 
 module.exports = function(app) {
@@ -28,8 +31,7 @@ module.exports = function(app) {
   //app.route('/auth/resetForgotPassword')
   //    .put(extensionUserController.resetForgotPassword);
 
-  app.route('/auth/profile')
-    .put(extensionUserController.updateProfile);
+
 
   app.route('/auth/user/:userId')
       .get(extensionUserController.getUserById);
@@ -57,5 +59,31 @@ module.exports = function(app) {
   
   app.route('/auth/guest/to/user/signin')
       .post(extensionUserController.signInUserWithGuestUserItems);
+
+  // Setting up the users password api
+  app.route('/auth/user/password')
+      .post(exUserPasswordController.changePassword);
+  app.route('/auth/user/forgot')
+      .post(exUserPasswordController.forgot);
+  app.route('/auth/user/reset/:token')
+      .get(exUserPasswordController.validateResetToken);
+  app.route('/auth/user/reset/:token')
+      .post(exUserPasswordController.reset);
+
+  // Setting up the users authentication api
+  app.route('/auth/user/signup')
+      .post(exUserAuthenticationController.signup);
+  app.route('/auth/user/signin')
+      .post(exUserAuthenticationController.signin);
+  app.route('/auth/user/signout')
+      .get(exUserAuthenticationController.signout);
+
+  // Setting up the users profile api
+  app.route('/auth/users/me')
+      .get(exUserProfileController.me);
+  app.route('/auth/user/profile')
+      .put(exUserProfileController.updateProfile);
+  app.route('/auth/users/accounts')
+      .delete(exUserAuthenticationController.removeOAuthProvider);
 
 };
