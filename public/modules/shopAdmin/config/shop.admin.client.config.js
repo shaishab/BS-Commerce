6a9998b085ca08cd4e121b2690d78240b1772058
Shaishab <shaishab.cse@gmail.com>
2016-05-27 20:19:01 +0600

    Add busy bar for admin panel to show every request

--- public/modules/shopAdmin/config/shop.admin.client.config.js
+++ public/modules/shopAdmin/config/shop.admin.client.config.js
@@ -4,14 +4,37 @@
 angular.module('shopAdmin').config(['$httpProvider',
     function($httpProvider) {
         // Set the httpProvider "not authorized" interceptor
-        $httpProvider.interceptors.push(['$q', '$location', 'Global',
-            function($q, $location, Global) {
+        $httpProvider.interceptors.push(['$q', '$location', 'Global', '$rootScope',
+            function($q, $location, Global, $rootScope) {
                 //console.log('Global data = ',Global.isAdmin);
                 if(!Global.user || !Global.isAdmin) {
                     $location.path('signin');
                 }
                 return {
+                    'request': function(config) {
+                        // do something on success
+                        $rootScope.isBusy = true;
+                        return config;
+                    },
+
+                    // optional method
+                    'requestError': function(rejection) {
+                        // do something on error
+                        $rootScope.isBusy = false;
+                        return $q.reject(rejection);
+                    },
+
+
+
+                    // optional method
+                    'response': function(response) {
+                        // do something on success
+                        $rootScope.isBusy = false;
+                        return response;
+                    },
+
                     responseError: function(rejection) {
+                        $rootScope.isBusy = false;
                         switch (rejection.status) {
                             case 401:
                                 // Deauthenticate the global user
