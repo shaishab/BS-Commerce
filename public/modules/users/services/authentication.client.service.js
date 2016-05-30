9dff22359870b856956f6f58076c6bc3f6d3c711
Shaishab <shaishab.cse@gmail.com>
2016-05-30 16:56:02 +0600

    Back to initial commit for this file

--- public/modules/users/services/authentication.client.service.js
+++ public/modules/users/services/authentication.client.service.js
@@ -12,23 +12,3 @@ angular.module('users').factory('Authentication', [
 		return _this._data;
 	}
 ]);
-
-//Global service for global variables
-angular.module('users').factory('Global', [
-
-	function() {
-		var _this = this;
-		_this._data = {
-			user: window.user,
-			authenticated: false,
-			isAdmin: false,
-			isRegistered: false
-		};
-		if (window.user && window.user.roles && window.user.roles.length) {
-			_this._data.authenticated = true;
-			_this._data.isAdmin = window.user.roles.indexOf('admin') !== -1;
-			_this._data.isRegistered = (window.user.roles.indexOf('admin') !== -1 || window.user.roles.indexOf('user') !== -1);
-		}
-		return _this._data;
-	}
-]);
